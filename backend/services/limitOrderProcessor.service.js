9b40a4b8dd45bd029fe648a2a8ebae8c82d8e2da
krish virani <viranikrish27@gmail.com>
2026-01-07 19:50:26 +0530

    added mongodb session in also sell mode

--- backend/services/limitOrderProcessor.service.js
+++ backend/services/limitOrderProcessor.service.js
@@ -77,29 +77,56 @@ module.exports.limitOrderProcessor = async () => {
 
         if (order.mode === "SELL" && currentPrice >= order.price) {
 
-            const lockedOrder = await OrderModel.findOneAndUpdate(
-                { _id: order._id, status: "PENDING" },
-                { status: "PROCESSING" }
-            );
 
-            // if lockedOrder is false means order process by another cycle.
-            if (!lockedOrder) continue;
-
+            let session;
             try {
 
+                session = await mongoose.startSession();
+                session.startTransaction();
+
+                const lockedOrder = await OrderModel.findOneAndUpdate(
+                    { _id: order._id, status: "PENDING" },
+                    { status: "PROCESSING" },
+                    { session }
+                );
+
+                // if lockedOrder is false means order process by another cycle.
+                if (!lockedOrder) continue;
+
                 // stock reduce from holding
-                await executeSell(lockedOrder, currentPrice);
+                await executeSell(lockedOrder, currentPrice, session);
 
                 // marks as order executed
-                lockedOrder.status = "EXECUTED";
-                lockedOrder.executedPrice = currentPrice;
-                lockedOrder.executedAt = new Date();
-                await lockedOrder.save();
+                await OrderModel.updateOne(
+                    { _id: lockedOrder._id },
+                    {
+                        $set: {
+                            status: "EXECUTED",
+                            executedPrice: currentPrice,
+                            executedAt: new Date()
+                        }
+                    },
+                    { session }
+                );
+
+                await session.commitTransaction();
+                session.endSession();
 
             } catch (error) {
 
-                lockedOrder.status = "FAILED";
-                await lockedOrder.save();
+                if (session) {
+                    await session.abortTransaction();
+                    session.endSession();
+                }
+
+                await OrderModel.updateOne(
+                    { _id: order._id },
+                    {
+                        $set: {
+                            status: "FAILED"
+                        }
+                    }
+                );
 
             }
 
@@ -111,7 +138,7 @@ module.exports.limitOrderProcessor = async () => {
 
 async function executeBuy(order, executedPrice, session) {
 
-    const holding = await HoldingModel.findOne({ symbol: order.symbol, user: order.user },null, { session });
+    const holding = await HoldingModel.findOne({ symbol: order.symbol, user: order.user }, null, { session });
 
     // if stock purches second time
     if (holding) {
@@ -137,9 +164,9 @@ async function executeBuy(order, executedPrice, session) {
 
 }
 
-async function executeSell(order, executedPrice) {
+async function executeSell(order, executedPrice, session) {
 
-    const holding = await HoldingModel.findOne({ symbol: order.symbol, user: order.user });
+    const holding = await HoldingModel.findOne({ symbol: order.symbol, user: order.user }, null, { session });
 
     if (!holding || holding.qty < order.qty) {
         throw new Error("Invalid SELL execution");
@@ -148,9 +175,9 @@ async function executeSell(order, executedPrice) {
     holding.qty -= order.qty;
 
     if (holding.qty === 0) {
-        await HoldingModel.deleteOne({ symbol, user: order.user });
+        await HoldingModel.deleteOne({ symbol, user: order.user }, { session });
     } else {
-        await holding.save();
+        await holding.save({ session });
     }
 
 }
